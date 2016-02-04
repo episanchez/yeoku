bb152699b3c5354fe05903567a475cdbefa04ce7
episanchez <charlie.quillard@epitech.eu>
2016-02-04 21:33:22 +0800

    fix Array variable (Aspect) and add index module

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    World    : require('./lib/world'),
+    Entity : require('./lib/entity'),
+    EntityManager    : require('./lib/entityManager'),
+    System    : require('./lib/system/baseSystem'),
+};
