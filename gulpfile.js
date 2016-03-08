216dfd5274284581d6ca5e433188839643779c33
episanchez <charlie.quillard@epitech.eu>
2016-03-08 09:19:00 +0800

    add mocha task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp');
 var jsdoc = require('gulp-jsdoc3');
- 
+var mocha = require('gulp-mocha');
+
 gulp.task('doc', function (cb) {
     var config = require('./conf.json');
     gulp.src(['README.md', './lib/**/*.js'], {read: false})
         .pipe(jsdoc(config, cb));
 });
+
+gulp.task('mocha', () => {
+    return gulp.src('test/*.js')
+        .pipe(mocha())
+        .once('error', () => {
+            process.exit(1);
+        })
+        .once('end', () => {
+            process.exit();
+        });
+});
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['doc']);
